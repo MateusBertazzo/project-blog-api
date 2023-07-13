25b950898e419a77b5f8028b3b014d3d8a0ad473
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-07-13 16:20:32 -0300

    cria model de User

---
+++ src/models/User.js
@@ -0,0 +1,34 @@
+modulo.exports = (sequelize, DataTypes) => {
+  const tableUser = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    display_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, 
+  {
+    timestamps: false,
+    underscored: true,
+    tableName: 'users'
+  })
+
+  return tableUser;
+}
