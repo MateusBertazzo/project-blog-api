cae7eef9b313d2bf169a99f5e1034f691c74df3c
Mateus Bertazzo <mateusbataglin52@gmail.com>
2023-07-13 18:14:03 -0300

    implementa rota de userLogin/middlewares e resolve req 3

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { userRoute } = require('./routes');
 // ...
 
 const app = express();
@@ -10,6 +10,8 @@ app.get('/', (_request, response) => {
 });
 
 app.use(express.json());
+
+app.use('/login', userRoute);
 
 // ...
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,32 @@
 const jwt = require('jsonwebtoken');
 const { userService } = require('../services');
 
-const secret = 'mySecret';
+const secret = process.env.JWT_SECRET || 'secret';
 const config = {
   algorithm: 'HS256',
 };
 
-const TokenUser = (result) => {
-  jwt.sign(result, secret, config);
+const TokenUser = (result) => jwt.sign(result, secret, config);
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const { type, message } = await userService.loginUser(email, password);
+
+  if (type) {
+    return res.status(400).json({ message });
+  }
+
+  const payload = {
+    email: message.email,
+    id: message.id,
+  };
+
+  const token = TokenUser(payload);
+
+  res.status(200).json({ token });
+};
+
+module.exports = {
+  loginUser,
 };
